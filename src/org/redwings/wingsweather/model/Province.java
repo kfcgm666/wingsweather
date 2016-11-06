c33be85dda4ac1ac7e9f0c4d41351408f193dc7a
gm <kfcgm666@gmail.com>
2016-11-06 23:46:18 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/org/redwings/wingsweather/model/Province.java
@@ -0,0 +1,34 @@
+package org.redwings.wingsweather.model;
+
+public class Province {
+	
+	private int id;
+	private String provinceName;
+	private String provinceCode;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getProvinceName() {
+		return provinceName;
+	}
+	
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+	
+	public String getProviceCode() {
+		return provinceCode;
+	}
+	
+	public void setProvinceCode(String provinceCode) {
+		this.provinceCode = provinceCode;
+	}
+	
+	
+}
